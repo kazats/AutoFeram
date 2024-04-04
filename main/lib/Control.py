63625b193446fa91007a0f4aff2e0736eb164ea0
Kazatsu Kazu <kazatsukazu@gmail.com>
2024-04-04 20:31:22 +0200

    cleaned up control_temperature

--- main/lib/Control.py
+++ main/lib/Control.py
@@ -14,33 +14,33 @@ def control_temperature(
     Tf: int,
     dT: int
     ):
-    os.makedirs(Path.cwd() / 'dipoRavg', exist_ok=True) # sp.call("mkdir dipoRavg", shell=True)
-    os.makedirs(Path.cwd() / 'coords', exist_ok=True) # sp.call("mkdir coords", shell=True)
+    os.makedirs(Path.cwd() / 'dipoRavg', exist_ok=True)
+    os.makedirs(Path.cwd() / 'coords', exist_ok=True)
 
     for temperature in range(Ti, Tf, dT):
-        config.setup.kelvin = temperature # setattr(config.setup, 'kelvin', temperature)
-        print(asdict(config.setup))
+        avg_file        = Path.cwd() / f'{sim_name}.avg'
+        thermo_file     = Path.cwd() / 'thermo.avg'
+        dipoRavg_file   = Path.cwd() / f'{sim_name}.dipoRavg'
+        temp_dipoRavg_file = Path.cwd() / 'dipoRavg' / f'{temperature}.dipoRavg'
+        last_coord_file = Path.cwd() / f'{sim_name}.{config.last_coord()}.coord'
+        restart_file    = Path.cwd() / f'{sim_name}.restart'
+        temp_coord_file = Path.cwd() / 'coords' / f'{temperature}.coord'
+
+
+        config.setup.kelvin = temperature
         config.write_feram_file(sim_name)
 
-        avg_file    = Path.cwd() / f'{sim_name}.avg'
-        thermo_file = Path.cwd() / 'thermo.avg'
 
         sp.run([feram_bin, f'{sim_name}.feram'], check=True)
 
-        # sp.call(f"cat {name}.avg >> thermo.avg", shell=True)
+        # good?
         with open(avg_file, 'r') as inf,\
             open(thermo_file, 'a+') as outf:
-            outf.write(inf.read())
+            outf.write(inf.read())                      # sp.call(f"cat {name}.avg >> thermo.avg", shell=True)
 
-        os.remove(avg_file) # sp.call(f"rm {name}.avg", shell=True)
-        os.rename(f'{sim_name}.dipoRavg', Path.cwd() / 'dipoRavg' / f'{temperature}.dipoRavg') # sp.call(f"mv {sim_name}.dipoRavg ./dipoRavg/{temperature}.dipoRavg", shell=True)
-
-        # sp.call(f"cp ./{sim_name}.{config.last_coord()}.coord ./{sim_name}.restart")
-        shutil.copy2(Path.cwd() / f'{sim_name}.{config.last_coord()}.coord',
-                     Path.cwd() / f'{sim_name}.restart',)
-
-        # sp.call(f"mv ./{sim_name}.{config.last_coord()}.coord ./coords/{temperature}.coord", shell=True)
-        os.rename(Path.cwd() / f'{sim_name}.{config.last_coord()}.coord',
-                  Path.cwd() / 'coords' / f'{temperature}.coord') # sp.call(f"mv {sim_name}.dipoRavg ./dipoRavg/{temperature}.dipoRavg", shell=True)
+        os.remove(avg_file)                             # sp.call(f"rm {name}.avg", shell=True)
+        os.rename(dipoRavg_file, temp_dipoRavg_file)    # sp.call(f"mv {sim_name}.dipoRavg ./dipoRavg/{temperature}.dipoRavg", shell=True)
+        shutil.copy2(last_coord_file, restart_file)     # sp.call(f"cp ./{sim_name}.{config.last_coord()}.coord ./{sim_name}.restart")
+        os.rename(last_coord_file, temp_coord_file)     # sp.call(f"mv ./{sim_name}.{config.last_coord()}.coord ./coords/{temperature}.coord", shell=True)
 
     # spb.call(f"rm {NAME}.restart", shell=True)
