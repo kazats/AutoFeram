ab7b8be638bf946b330ef13e34850963e9ce2892
fighting-mochi <lantienhsu@gmail.com>
2024-04-04 21:49:48 +0200

    modified main.py using SetupStaticElecField

--- main/lib/main.py
+++ main/lib/main.py
@@ -7,16 +7,17 @@ import Control
 
 if __name__ == "__main__":
     SIM_NAME = 'bto'
-    # FERAM_BIN = Path.home() / 'Code' / 'git' / 'AutoFeram' / 'feram-0.26.04' / 'build_20240401' / 'src' / 'feram'
-    FERAM_BIN = Path('feram')
+    FERAM_BIN = Path.home() / 'Code' / 'git' / 'AutoFeram' / 'feram-0.26.04' / 'build_20240401' / 'src' / 'feram'
+    # FERAM_BIN = Path('feram')
 
     config = Config.FeramConfig(
-        setup = Config.Setup(
+        setup = Config.SetupStaticElecField(
             verbose      = 1,
             L            = '2 2 2',
             n_thermalize = 1,
             n_average    = 4,
             n_coord_freq = 1,
+            external_E_field = '0.001 0 0',
         ),
         material = BTO
     )
