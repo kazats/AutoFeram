58b2e1b26b1e89e86751118c77d48af6bc7d1020
Kazatsu Kazu <kazatsukazu@gmail.com>
2024-05-19 17:30:08 +0200

    refactor Temperature

--- src/lib/control/Temperature.py
+++ src/lib/control/Temperature.py
@@ -12,41 +12,54 @@ from src.lib.materials.BTO import BTO
 from src.lib.Config import *
 from src.lib.Operations import *
 from src.lib.Log import *
-from src.lib.Util import project_root
+from src.lib.Util import feram_with_fallback, project_root
 
 
-def run(
-    sim_name: str,
-    feram_bin: Path,
-    dst: Path,
-    config: Config.FeramConfig,
-    Ti: int,
-    Tf: int,
-    dT: int
-    ) -> Result[Any, str]:
+class TempRunner(NamedTuple):
+    sim_name: str
+    working_dir: Path
+    feram_path: Path
 
-    feram_file      = dst / f'{sim_name}.feram'
-    avg_file        = dst / f'{sim_name}.avg'
-    thermo_file     = dst / 'thermo.avg'
-    dipoRavg_file   = dst / f'{sim_name}.dipoRavg'
-    last_coord_file = dst / f'{sim_name}.{config.last_coord()}.coord'
-    restart_file    = dst / f'{sim_name}.restart'
 
-    res = OperationSequence([
-        MkDirs(DirOut(dst / 'dipoRavg')),
-        MkDirs(DirOut(dst / 'coords'))
-    ]).run()
+class Temp(NamedTuple):
+    initial: int
+    final: int
+    delta: int
 
-    if is_err(res):
-        return res
 
-    for temperature in range(Ti, Tf, dT):
-        temp_dipoRavg_file = dst / 'dipoRavg' / f'{temperature}.dipoRavg'
-        temp_coord_file    = dst / 'coords' / f'{temperature}.coord'
+class TempConfig(NamedTuple):
+    config: FeramConfig
+    temperatures: Temp
+    # Ti: int
+    # Tf: int
+    # dT: int
+
+
+def run(runner: TempRunner, temp_config: TempConfig) -> Result[Any, str]:
+
+    sim_name, working_dir, feram_bin = runner
+    config, temps = temp_config
+
+    feram_file      = working_dir / f'{sim_name}.feram'
+    avg_file        = working_dir / f'{sim_name}.avg'
+    thermo_file     = working_dir / 'thermo.avg'
+    dipoRavg_file   = working_dir / f'{sim_name}.dipoRavg'
+    last_coord_file = working_dir / f'{sim_name}.{config.last_coord()}.coord'
+    restart_file    = working_dir / f'{sim_name}.restart'
+
+    pre = OperationSequence([
+        MkDirs(DirOut(working_dir / 'coords')),
+        MkDirs(DirOut(working_dir / 'dipoRavg')),
+        Cd(DirIn(working_dir))
+    ])
+
+    def step(temperature: int) -> OperationSequence:
+        temp_dipoRavg_file = working_dir / 'dipoRavg' / f'{temperature}.dipoRavg'
+        temp_coord_file    = working_dir / 'coords' / f'{temperature}.coord'
 
         config.setup['kelvin'] = temperature
 
-        res = OperationSequence([
+        return OperationSequence([
             Write(FileOut(feram_file),
                   config.generate_feram_file),
             Feram(Exec(feram_bin),
@@ -60,14 +73,21 @@ def run(
                  FileOut(restart_file)),
             Rename(FileIn(last_coord_file),
                    FileOut(temp_coord_file)),
-        ]).run()
+        ])
 
-        if is_err(res):
-            return res
+    steps = reduce(lambda acc, t: acc + step(t), range(*temps), OperationSequence())
 
-    Remove(FileIn(restart_file)).run()
+    post = OperationSequence([
+        Remove(FileIn(restart_file))
+    ])
 
-    return Ok('Control Temperature: success')
+    all = OperationSequence([
+        *pre,
+        *steps,
+        *post
+    ])
+
+    return all.run().and_then(lambda _: Ok('Control Temperature: success'))
 
 
 def post_process(log: Log, config: FeramConfig) -> pd.DataFrame:
@@ -82,37 +102,42 @@ def post_process(log: Log, config: FeramConfig) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    SIM_NAME = 'bto'
-    # FERAM_BIN = Path.home() / 'Code' / 'git' / 'AutoFeram' / 'feram-0.26.04' / 'build_20240401' / 'src' / 'feram'   # FERAM_BIN = Path('feram')
-    FERAM_BIN = Path(cast(str, shutil.which('feram')))
+    CUSTOM_FERAM_BIN = Path.home() / 'Code' / 'git' / 'AutoFeram' / 'feram-0.26.04' / 'build_20240401' / 'src' / 'feram'
 
 
     test_path = project_root() / 'output' / 'temp'
     os.makedirs(test_path, exist_ok=True)
 
-    config = FeramConfig(
-        setup = merge_setups([
-            General(
-                verbose      = 4,
-                L            = Vec3(2, 2, 2),
-                n_thermalize = 1,
-                n_average    = 4,
-                n_coord_freq = 1,
-                bulk_or_film = Structure.Epit
-            ),
-            EFieldStatic(
-                external_E_field = Vec3(0.001, 0, 0)
-            ),
-            Strain(
-                epi_strain = Vec3(0.01, 0.01, 0)
-            )
-        ]),
-        material = BTO
+    runner = TempRunner(
+        sim_name    = 'bto',
+        feram_path  = feram_with_fallback(CUSTOM_FERAM_BIN),
+        working_dir = project_root() / 'output' / 'temp',
     )
 
-    res = run(SIM_NAME, FERAM_BIN,
-              test_path, config,
-              Ti=10, Tf=20, dT=5)
+    config = TempConfig(
+        FeramConfig(
+            setup = merge_setups([
+                General(
+                    verbose      = 4,
+                    L            = Vec3(2, 2, 2),
+                    n_thermalize = 1,
+                    n_average    = 4,
+                    n_coord_freq = 1,
+                    bulk_or_film = Structure.Epit
+                ),
+                EFieldStatic(
+                    external_E_field = Vec3(0.001, 0, 0)
+                ),
+                Strain(
+                    epi_strain = Vec3(0.01, 0.01, 0)
+                )
+            ]),
+            material = BTO
+        ),
+        temperatures = Temp(initial=10, final=20, delta=5)
+    )
+
+    res = run(runner, config)
 
     color_res = colors.yellow(res) if res.is_ok() else colors.red(res)
     print(color_res)
@@ -121,7 +146,7 @@ if __name__ == "__main__":
     log_path = test_path / 'bto.log'
     log_raw  = read_log(log_path)
     log      = parse_log(log_raw)
-    res      = post_process(log, config)
+    res      = post_process(log, config.config)
 
     print(res)
     # print(len(res))
