091c2b4b2db67f4f3a83f064cd25ede73cdd76f5
Kazatsu Kazu <kazatsukazu@gmail.com>
2024-05-20 14:57:55 +0200

    refactor

--- src/lib/Domain.py
+++ src/lib/Domain.py
@@ -1,66 +1,51 @@
 #!/usr/bin/env python
 # author: Lan-Tien Hsu
 
-
 '''
-Define your domain seeds (variables called "Domain", imagine a seed where a specific domain starts to grow) and the electric field in xyz direction (variables called "Props") in the __main__ function in the domain_rectangle.py.
+Define your domain seeds (variables called "Domain", imagine a seed where a specific domain starts to grow)
+and the electric field in xyz direction (variables called "Props") in the __main__ section.
 '''
 
-from typing import Any, List, Tuple, Dict, Iterator, Optional
-from dataclasses import dataclass, astuple
-from collections import Counter
+from collections.abc import Sequence
 import numpy as np
+from pathlib import Path
+from dataclasses import dataclass
+from collections import Counter
+from typing import NamedTuple, TypeAlias, Iterator, Optional
+
+from src.lib.common import Vec3
+from src.lib.Util import project_root
 
 
-Vec3 = Tuple[int, int, int]
+Int3:  TypeAlias = Vec3[int]
+Props: TypeAlias = Vec3[float]
 
-
-@dataclass(frozen=True)
-class Props:
-    x: float
-    y: float
-    z: float
-
-    def __iter__(self) -> Iterator[Any]:
-        return iter(astuple(self))
-
-    def __repr__(self):
-        return f"(Prop {self.x} {self.y} {self.z})"
-
-
-@dataclass(frozen=True)
-class Domain:
-    seed: Vec3
+class Domain(NamedTuple):
+    seed: Int3
     props: Props
 
-    def __repr__(self):
-        return f"(Domain {self.seed} {self.props})"
-
-
-@dataclass
-class Point:
+class PointProps(NamedTuple):
     domain: Domain
     boundary: Optional[float] = None
 
-    def __repr__(self):
-        return f"(Point {self.domain} {self.boundary})"
-
+Point:    TypeAlias = tuple[Int3, PointProps]
+PointMap: TypeAlias = dict[Int3, PointProps]
 
 @dataclass
 class System:
-    size: Vec3
-    points: Dict[Vec3, Point]
+    size: Int3
+    points: PointMap
 
     def __len__(self) -> int:
         return len(self.points)
 
-    def __iter__(self) -> Iterator[Tuple[Vec3, Point]]:
+    def __iter__(self) -> Iterator[Point]:
         return iter(self.points.items())
 
-    def __getitem__(self, coord: Vec3) -> Point:
+    def __getitem__(self, coord: Int3) -> PointProps:
         return self.points[coord]
 
-    def find_neighbors(self, coord: Vec3, d: int) -> List[Tuple[Vec3, Point]]:
+    def find_neighbors(self, coord: Int3, d: int) -> list[Point]:
         """right now, it only works for d = 1"""
 
         """
@@ -80,13 +65,13 @@ class System:
         """
 
         x, y, z = coord
-        coords: List[Vec3] = [
-            (x - d, y, z),
-            (x, y - d, z),
-            (x, y, z - d),
-            (x + d, y, z),
-            (x, y + d, z),
-            (x, y, z + d),
+        coords: list[Int3] = [
+            Int3(x - d, y, z),
+            Int3(x, y - d, z),
+            Int3(x, y, z - d),
+            Int3(x + d, y, z),
+            Int3(x, y + d, z),
+            Int3(x, y, z + d),
         ]
 
         return [
@@ -96,82 +81,92 @@ class System:
                 and all(coord[i] < self.size[i] for i in range(len(self.size)))
         ]
 
-    def find_most_common_grain(self, coord: Vec3, d: int) -> Tuple[Domain, float]:
+    def find_most_common_grain(self, coord: Int3, d: int) -> tuple[Domain, float]:
         """float: is the percentage of the majority domain"""
 
-        neighbors: List[Tuple[Vec3, Point]] = self.find_neighbors(coord, d)
-
-        neighbor_grains: List[Domain] = [n[1].domain for n in neighbors]
-
-        neighbor_grain_count = Counter(neighbor_grains)
-
-        max_grain = neighbor_grain_count.most_common(1)[0]
+        neighbors: Sequence[Point]    = self.find_neighbors(coord, d)
+        neighbor_grains: list[Domain] = [n[1].domain for n in neighbors]
+        neighbor_grain_count          = Counter(neighbor_grains)
+        max_grain                     = neighbor_grain_count.most_common(1)[0]
 
         return (max_grain[0], max_grain[1] / neighbor_grain_count.total())
 
-    def find_boundary(self, coord: Vec3, d: int) -> Tuple[Domain, float]:
+    def find_boundary(self, coord: Int3, d: int) -> tuple[Domain, float]:
         return self.find_most_common_grain(coord, d)
 
 
-def find_closest_grain(grains: List[Domain], coord: Vec3) -> Domain:
-    def distance(p1: Vec3, p2: Vec3) -> np.floating:
+def find_closest_grain(domains: list[Domain], coord: Int3) -> Domain:
+    def distance(p1: Int3, p2: Int3) -> np.floating:
         return np.linalg.norm(np.fromiter(p1, int) - np.fromiter(p2, int))
 
     return min(
-        map(lambda domain: (domain, distance(domain.seed, coord)), grains),
-        key=lambda x: float(x[1]),
+        map(lambda domain: (domain, distance(domain.seed, coord)),
+            domains),
+        key=lambda x: x[1],
     )[0]
 
 
-def generate_coords(size: Vec3) -> List[Vec3]:
+def generate_coords(size: Int3) -> list[Int3]:
     size_x, size_y, size_z = map(range, size)
-    return [(x, y, z) for x in size_x for y in size_y for z in size_z]
+    return [
+        Int3(x, y, z)
+        for x in size_x
+        for y in size_y
+        for z in size_z
+    ]
 
 
-def find_boundaries(size: Vec3, grains: List[Domain]) -> Dict[Vec3, Point]:
-    coords: List[Vec3] = generate_coords(size)
+def find_boundaries(size: Int3, grains: list[Domain]) -> PointMap:
+    coords: list[Int3] = generate_coords(size)
     system: System = System(
         size,
-        {coord: Point(find_closest_grain(grains, coord), None) for coord in coords},
+        {coord: PointProps(find_closest_grain(grains, coord), None) for coord in coords},
     )
 
     boundaries = {
-        coord: Point(*system.find_boundary(coord, 1)) for coord in system.points.keys()
+        coord: PointProps(*system.find_boundary(coord, 1)) for coord in system.points.keys()
     }
 
     return boundaries
 
 
-def bto_localfield(system: Dict[Vec3, Point]):
-    with open("bto.localfield", "w") as f:
+def write_bto_localfield(dir_out: Path, system: PointMap):
+    with open(dir_out / 'bto.localfield', 'w') as f:
         for coord, point in system.items():
             x, y, z = coord
             px, py, pz = point.domain.props
-            f.write(f"{x} {y} {z} {px} {py} {pz}\n")
+            f.write(f'{x} {y} {z} {px} {py} {pz}\n')
 
 
-def bto_defects(system: Dict[Vec3, Point]):
-    with open("bto.defects", "w") as f:
+def write_bto_defects(dir_out: Path, system: dict[Int3, PointProps]):
+    with open(dir_out / 'bto.defects', 'w') as f:
         for coord, point in system.items():
             x, y, z = coord
             px, py, pz = point.domain.props
 
-            if point.boundary != None and point.boundary < 1:
-                f.write(f"{x} {y} {z} {px * 134.106} {py} {pz}\n")
+            if point.boundary and point.boundary < 1:
+                f.write(f'{x} {y} {z} {px * 134.106} {py} {pz}\n')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    working_dir = project_root() / 'output' / 'domain'
 
-    size = (48, 96, 48)
+    # size = Int3(48, 96, 48)
+    # grains = [
+    #     Domain(Int3(24, 0, 0), Props(-1, 0, 0)),
+    #     Domain(Int3(0, 48, 0), Props(0, 1, 0)),
+    #     Domain(Int3(24, 95, 0), Props(1, 0, 0)),
+    #     Domain(Int3(48, 48, 0), Props(0, -1, 0)),
+    # ]
 
+    size = Int3(24, 48, 24)
     grains = [
-        Domain((24, 0, 0), Props(-1, 0, 0)),
-        Domain((0, 48, 0), Props(0, 1, 0)),
-        Domain((24, 95, 0), Props(1, 0, 0)),
-        Domain((48, 48, 0), Props(0, -1, 0)),
+        Domain(Int3(12, 0, 0), Props(-1, 0, 0)),
+        Domain(Int3(0, 24, 0), Props(0, 1, 0)),
+        Domain(Int3(12, 47, 0), Props(1, 0, 0)),
+        Domain(Int3(24, 24, 0), Props(0, -1, 0)),
     ]
 
     system = find_boundaries(size, grains)
 
-    [ f(system) for f in [bto_localfield, bto_defects] ]
-
+    [ f(working_dir, system) for f in [write_bto_localfield, write_bto_defects] ]
