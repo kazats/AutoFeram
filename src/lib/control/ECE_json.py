5a11c6d371779d357386db32fd4dea6b5039f272
Kazatsu Kazu <kazatsukazu@gmail.com>
2024-08-18 00:01:25 +0200

    use feram json output

---
+++ src/lib/control/ECE_json.py
@@ -0,0 +1,183 @@
+import datetime
+import polars as pl
+from pathlib import Path
+from functools import reduce
+from itertools import accumulate, zip_longest
+from collections.abc import Mapping
+from typing import NamedTuple
+
+from src.lib.control.common import Runner, LOG_SCHEMA
+from src.lib.common import BoltzmannConst, Vec3, colorize
+from src.lib.materials.BTO import BTO
+from src.lib.Config import *
+from src.lib.Domain import *
+from src.lib.Operations import *
+from src.lib.Ovito import WriteOvitoDump
+from src.lib.Util import feram_with_fallback, project_root
+
+
+class ECEConfig(NamedTuple):
+    # (n_thermalize + n_average) % n_coord_freq must == 0
+    material: Material
+    steps:    Mapping[str, SetupDict]
+
+
+def run(runner: Runner, ece_config: ECEConfig) -> Result[Any, str]:
+
+    def setup_with(setup: SetupDict) -> FeramConfig:
+        return FeramConfig(
+            setup    = setup,
+            material = ece_config.material
+        )
+
+    sim_name, working_dir, feram_bin = runner
+
+    pre = OperationSequence([
+        MkDirs(DirOut(working_dir, preconditions=[dir_doesnt_exist])),
+        *[MkDirs(DirOut(working_dir / step_dir)) for step_dir in ece_config.steps.keys()]
+    ])
+
+    def step(setup: SetupDict, dir_cur: Path, dir_next: Path) -> OperationSequence:
+        config          = setup_with(setup)
+        feram_file      = dir_cur / f'{sim_name}.feram'
+        last_coord_file = dir_cur / f'{sim_name}.{config.last_coord()}.coord'
+        copy_restart    = Copy(FileIn(last_coord_file),
+                               FileOut(dir_next / f'{sim_name}.restart')) if dir_next is not Any else Empty()
+
+        return OperationSequence([
+            Write(FileOut(feram_file),
+                  config.generate_feram_file),
+            WithDir(DirIn(working_dir),
+                    DirIn(dir_cur),
+                    Feram(Exec(feram_bin),
+                          FileIn(feram_file))),
+            copy_restart,
+            WriteOvitoDump(FileOut(working_dir / f'coords_{dir_cur.name}.ovt'),
+                           DirIn(dir_cur),
+                           'coord'),
+            WriteOvitoDump(FileOut(working_dir / f'dipoRavgs_{dir_cur.name}.ovt'),
+                           DirIn(dir_cur),
+                           'dipoRavg')
+        ])
+
+    def reducer(acc: OperationSequence, next_step) -> OperationSequence:
+        (dir_cur, setups), (dir_next, _) = next_step
+        return acc + step(setups, dir_cur, dir_next)
+
+    steps     = [(working_dir / step_dir, setups) for step_dir, setups in ece_config.steps.items()]
+    step_zip  = zip_longest(steps, steps[1:], fillvalue=(Any, Any))
+    steps_all = reduce(reducer, step_zip, OperationSequence())
+
+    post = OperationSequence([
+        Copy(FileIn(Path(__file__)),
+             FileOut(working_dir / 'AutoFeram_control.py')),
+        WriteParquet(FileOut(working_dir / f'{working_dir.name}.parquet'),
+                     lambda: post_process(runner, ece_config)),
+        Archive(DirIn(working_dir),
+                FileOut(project_root() / 'output' / f'{working_dir.name}.tar.gz'))
+    ])
+
+    all = OperationSequence([
+        pre,
+        steps_all,
+        post
+    ])
+
+    return all.run().and_then(
+        lambda _: Ok('ECE: Success')).map_err(
+        lambda _: 'ECE: Failure')
+
+
+def post_process(runner: Runner, config: ECEConfig) -> pl.DataFrame:
+    sim_name, working_dir, _ = runner
+    json_name = f'{sim_name}.json'
+
+    def mk_df(step_dir: str, setup: SetupDict) -> pl.DataFrame:
+        df = pl.read_json(working_dir / step_dir / json_name, schema = LOG_SCHEMA)
+
+        return df.with_columns(
+            step  = pl.lit(step_dir),
+            dt_fs = pl.lit(setup['dt'] * 1000)
+        )
+
+    merged_df = pl.concat([mk_df(step_dir, setup) for step_dir, setup in config.steps.items()])
+    time      = accumulate(merged_df['dt_fs'], lambda acc, x: acc + x)
+
+    return merged_df.with_columns(
+        time_fs = pl.Series(time),
+        kelvin  = pl.col('dipo_kinetic') / (1.5 * BoltzmannConst)
+    )
+
+
+if __name__ == "__main__":
+    CUSTOM_FERAM_BIN = Path.home() / 'feram_dev/build/src/feram'
+
+    material       = BTO
+    size           = Int3(2, 2, 2)
+    temperature    = 200
+    efield_initial = Vec3(0.001, 0, 0)
+    efield_final   = Vec3[float](0, 0, 0)
+    efield_static  = EFieldStatic(external_E_field = efield_initial)
+
+    common = {
+        'L':      size,
+        'kelvin': temperature,
+    }
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    runner = Runner(
+        sim_name    = 'bto',
+        feram_path  = feram_with_fallback(),
+        working_dir = project_root() / 'output' / f'ece_json_{timestamp}',
+    )
+
+    _, working_dir, _ = runner
+
+    config = ECEConfig(
+        material = material,
+        steps = {
+            '1_preNPT': merge_setups([
+                General(
+                    method       = Method.MD,
+                    n_thermalize = 0,
+                    n_average    = 8, #0000
+                    n_coord_freq = 2, #0000
+                ),
+                efield_static
+            ]) | common,
+            '2_preNPE': merge_setups([
+                General(
+                    method       = Method.LF,
+                    n_thermalize = 0,
+                    n_average    = 12, #0000
+                    n_coord_freq = 2, #0000
+                ),
+                efield_static
+            ]) | common,
+            '3_rampNPE': merge_setups([
+                General(
+                    method       = Method.LF,
+                    n_thermalize = 10, #0000
+                    n_average    = 0,
+                    n_coord_freq = 2, #0000
+                ),
+                EFieldDynamic(
+                    n_hl_freq        = 1, #00
+                    n_E_wave_period  = 4, #100000,
+                    E_wave_type      = EWaveType.RampOff,
+                    external_E_field = efield_initial
+                )
+            ]) | common,
+            '4_postNPE': merge_setups([
+                General(
+                    method       = Method.LF,
+                    n_thermalize = 0,
+                    n_average    = 18, #0000
+                    n_coord_freq = 2, #0000
+                ),
+                efield_static
+            ]) | common
+        })
+
+    print(colorize(run(runner, config)))
--- src/lib/control/common.py
+++ src/lib/control/common.py
@@ -1,3 +1,4 @@
+import polars as pl
 from pathlib import Path
 from typing import NamedTuple
 
@@ -6,3 +7,27 @@ class Runner(NamedTuple):
     sim_name: str
     working_dir: Path
     feram_path: Path
+
+
+LOG_SCHEMA = {
+    'time_step':       pl.Int64,
+    'acou_kinetic':    pl.Float64,
+    'dipo_kinetic':    pl.Float64,
+    'short_range':     pl.Float64,
+    'long_range':      pl.Float64,
+    'dipole_E_field':  pl.Float64,
+    'unharmonic':      pl.Float64,
+    'homo_strain':     pl.Float64,
+    'homo_coupling':   pl.Float64,
+    'inho_strain':     pl.Float64,
+    'inho_coupling':   pl.Float64,
+    'inho_modulation': pl.Float64,
+    'total_energy':    pl.Float64,
+    'H_Nose_Poincare': pl.Float64,
+    's_Nose':          pl.Float64,
+    'pi_Nose':         pl.Float64,
+    'u':               pl.List(pl.Float64),
+    'u_sigma':         pl.List(pl.Float64),
+    'p':               pl.List(pl.Float64),
+    'p_sigma':         pl.List(pl.Float64),
+}
